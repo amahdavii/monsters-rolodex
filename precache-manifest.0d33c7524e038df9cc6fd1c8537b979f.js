967eb107a7352951ce869e3302508f4822671edb
Ali Mahdavi Amiri <amahdavi9978@gmail.com>
2020-02-20 10:20:16 +0330

    Updates

---
+++ precache-manifest.0d33c7524e038df9cc6fd1c8537b979f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1a3725a3569c57e6a9a289792bd2ad1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a61487653c32d5bcabdc",
+    "url": "/monsters-rolodex/static/css/main.4b22c2a8.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a61487653c32d5bcabdc",
+    "url": "/monsters-rolodex/static/js/main.ecaea524.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
